8c1a20d081ada5919cbfbe3b1e4a0fdff312725a
Maria Mempin <maria.mempin@mq.edu.au>
2019-07-25 15:37:14 +1000

    Assign value to area_hectares.

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -3,3 +3,20 @@ print("Hello, world")
 # Load required packages
 
 install.packages(tidyverse)
+
+area_hectares <- 1.0
+
+area_hectares
+
+area_acres <- 2.47 * area_hectares  # convert to acres
+
+area_hectares <- 2.5                # land area in hectares
+
+area_acres                          # print
+
+
+length <- 4
+width <- 6
+area <- length * width
+area
+
