6262bf5224a2cd64ec51aa00ba149625ca08a091
Maria Mempin <maria.mempin@mq.edu.au>
2019-07-25 15:06:49 +1000

    Data download safi openrefine

--- scripts/Data_Downloads.R
+++ scripts/Data_Downloads.R
@@ -7,3 +7,11 @@ download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/
 
 
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb")
+
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_openrefine.csv", destfile = "./data/SAFI_openrefine.csv")
+
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx", destfile = "./data/SAFI_messy.xlsx", mode = "wb")
+
+
