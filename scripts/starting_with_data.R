905c0cd500278f120ed081c5f350a181244e99f6
Maria Mempin <maria.mempin@mq.edu.au>
2019-07-26 09:34:07 +1000

    Start cleaning up data

---
+++ scripts/starting_with_data.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv", na = "NULL")
+interviews                                                   # Tibble shows part of the data
+
+view(interviews)
+
+
