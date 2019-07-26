c6dc15021492629a7f2160f43b57e311afa0d029
Maria Mempin <maria.mempin@mq.edu.au>
2019-07-26 15:20:07 +1000

    downloaded interviews_plotting

--- scripts/Data_Downloads.R
+++ scripts/Data_Downloads.R
@@ -15,3 +15,4 @@ download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx", destfile = "./data/SAFI_messy.xlsx", mode = "wb")
 
 
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/Interviews_plotting.rds", destfile = "./data/Interviews_plotting.rds")
